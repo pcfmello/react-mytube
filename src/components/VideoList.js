feddd8bf3310eb18a8537e3e3ad5427402848dde
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-02-22 22:58:22 -0300

    Seção 2, aula 27

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { List, Image, Dimmer, Loader } from "semantic-ui-react";
 
-const VideoList = () => (
-  <div className="video-list">
-    <h1>Video List</h1>
-  </div>
-);
-export default VideoList;
+class VideoList extends Component {
+  renderVideo = video => (
+    <List key={video.etag} animated verticalAlign="middle">
+      <List.Item>
+        <Image src={video.snippet.thumbnails.default.url} />
+        <List.Content>
+          <List.Header>{video.snippet.title}</List.Header>
+        </List.Content>
+      </List.Item>
+    </List>
+  );
+
+  render = () => (
+    <React.Fragment>
+      {this.props.loading && (
+        <Dimmer active>
+          <Loader size="medium">Carregando...</Loader>
+        </Dimmer>
+      )}
+      {!this.props.loading && (
+        <div className="video-list">
+          {this.props.videos.map(video => this.renderVideo(video))}
+        </div>
+      )}
+    </React.Fragment>
+  );
+}
+
+const mapStateToProps = state => ({
+  videos: state.find.videos,
+  loading: state.find.loading,
+  error: state.find.error
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(VideoList);
