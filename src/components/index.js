a263e9960ef465708f8a130b7e18ad5291057664
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-02-24 15:34:40 -0300

    Refatorando e melhorando o ajuste do layout

--- src/components/index.js
+++ src/components/index.js
@@ -1,40 +1,50 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-
 import { Grid } from "@material-ui/core";
 
 import SearchBar from "./SearchBar";
-import VideoList from "./VideoList";
 import VideoPlayer from "./VideoPlayer";
+import VideoList from "./VideoList";
 
 const styles = {
-  root: {
-    flexDirection: "column"
+  main: {
+    padding: "24px 36px"
   },
-  container: {
-    display: "flex",
-    flexDirection: "row"
-  },
-  videoList: {
-    justifyContent: "flex-end"
+  logo: {
+    width: "75%"
   }
 };
 
 const Main = ({ classes }) => (
-  <Grid container spacing={16} className={classes.root}>
-    <Grid item xs={12}>
-      <SearchBar />
+  <div className={classes.main}>
+    <Grid
+      container
+      direction="row"
+      justify="space-between"
+      alignItems="center"
+      spacing={24}
+    >
+      <Grid item xs={2}>
+        <img
+          className={classes.logo}
+          srcSet="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR79I8t6Agc7dlC72VoIzUwvDBvMNVTsM92QmcB1vPJMFJn68dX"
+          alt="Logo"
+        />
+      </Grid>
+      <Grid item xs={10}>
+        <SearchBar />
+      </Grid>
     </Grid>
-    <Grid container className={classes.container} spacing={16}>
+    <Grid container direction="row" spacing={24}>
       <Grid item md={8} xs={12}>
         <VideoPlayer />
       </Grid>
       <Grid item md={4} xs={12}>
-        <VideoList className={classes.videoList} />
+        <VideoList />
       </Grid>
     </Grid>
-  </Grid>
+  </div>
 );
 
 Main.propTypes = {
